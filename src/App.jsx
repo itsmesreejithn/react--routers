88370b6951cad77aeff96cf43fb9d92e98ccf0dc
Sreejith <jithupreethy@gmail.com>
2023-12-02 14:54:31 +0530

    finished implementing links

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./pages/Home";
 import Nopage from "./pages/Nopage";
+import Dashboard from "./pages/Dashboard";
+import Profile from "./pages/Profile";
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="login" element={<Login />} />
           <Route path="*" element={<Nopage />} />
         </Routes>
       </BrowserRouter>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Navbar, Nav, Button } from "react-bootstrap";
-
+import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" data-bs-theme="dark">
@@ -9,12 +9,20 @@ const Header = () => {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link>Home</Nav.Link>
-            <Nav.Link>Profile</Nav.Link>
-            <Nav.Link>Dash Board</Nav.Link>
-            <Button variant="outline-primary" className="ms-2">
-              LogIn
-            </Button>
+            <Link to="/" className="navLink">
+              <Nav.Link>Home</Nav.Link>
+            </Link>
+            <Link to="/profile" className="navLink">
+              <Nav.Link>Profile</Nav.Link>
+            </Link>
+            <Link to="/dashboard" className="navLink">
+              <Nav.Link>Dash Board</Nav.Link>
+            </Link>
+            <Link to="/login" className="navLink">
+              <Button variant="outline-primary" className="ms-2">
+                LogIn
+              </Button>
+            </Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
