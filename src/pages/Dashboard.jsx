5f81e82c12117704e35378277e85b10a13cd94f9
Sreejith <jithupreethy@gmail.com>
2023-12-03 11:23:51 +0530

    addded boilerplate of dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { Button, Card, Col, Container, Row } from "react-bootstrap";
+import Header from "../components/Header";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <Container fluid className="h-100">
+      <Header />
+      <Row>
+        <Col>
+          <Card>
+            <Card.Body>
+              <Card.Title>Welcome, user.name</Card.Title>
+              <Button variant="ouline-danger">Log out</Button>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default Dashboard;
